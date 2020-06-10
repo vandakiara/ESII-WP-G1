57716789c2de15af8287268c151ad8762759cc84
Franciele Faccin <hyrule@MacBook-Pro.lan>
2020-06-10 18:02:16 +0100

    Parse git diff into chunks

--- covid-evolution-diff/src/main/java/ConnGit.java
+++ covid-evolution-diff/src/main/java/ConnGit.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jgit.api.Git;
@@ -77,14 +79,30 @@ public class ConnGit {
 		}
 		for (DiffEntry entry : diff) {
 			System.out.println("Entry: " + entry + ", from: " + entry.getOldId() + ", to: " + entry.getNewId());
-			try (DiffFormatter formatter = new DiffFormatter(System.out)) {
+			OutputStream output = new OutputStream() {
+				private StringBuilder string = new StringBuilder();
+
+				@Override
+				public void write(int x) throws IOException {
+					this.string.append((char) x);
+				}
+
+				public String toString() {
+					return this.string.toString();
+				}
+			};
+			try (DiffFormatter formatter = new DiffFormatter(output)) {
 				formatter.setRepository(repository);
+
 				try {
 					formatter.format(entry);
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				System.out.println("Output: " + output);
+				System.out.println("Parser: ");
+				getDiffChunks(output.toString());
 			}
 		}
 
@@ -109,4 +127,56 @@ public class ConnGit {
 			return treeParser;
 		}
 	}
+
+	public List<List<GitDiffChunk>> getDiffChunks(String diff) {
+		List<List<GitDiffChunk>> chunks = new ArrayList<List<GitDiffChunk>>();
+		String[] diffStrs = diff.split("\n@@");
+		if (diffStrs.length < 2) {
+			return chunks;
+		}
+
+		for (int i = 1; i < diffStrs.length; i++) {
+			String diffBlock = diffStrs[i];
+			System.out.println("Block: " + diffBlock);
+			String[] lines = diffBlock.split("\n");
+			if (lines.length < 2) {
+				continue;
+			}
+			List<GitDiffChunk> chunk = new ArrayList<GitDiffChunk>();
+			int startDeleted = Integer.parseInt(lines[0].substring(lines[0].indexOf('-') + 1, lines[0].indexOf(',')));
+			int startAddition = Integer.parseInt(
+					lines[0].substring(lines[0].indexOf('+') + 1, lines[0].indexOf(',', lines[0].indexOf(',') + 1)));
+			int countDeleted = startDeleted;
+			int countAddition = startAddition;
+
+			for (int j = 1; j < lines.length; j++) {
+				String d = lines[j];
+				DiffType type = d.indexOf('-') == 0 ? DiffType.DELETION
+						: d.indexOf('+') == 0 ? DiffType.ADDITION : DiffType.NEUTRAL;
+				chunk.add(new GitDiffChunk(countDeleted, countAddition, type, d));
+				switch (type) {
+				case DELETION:
+					countDeleted++;
+					break;
+				case ADDITION:
+					countAddition++;
+					break;
+				default:
+					countDeleted++;
+					countAddition++;
+					break;
+				}
+			}
+			chunks.add(chunk);
+		}
+
+		for (List<GitDiffChunk> chunk : chunks) {
+			System.out.println("Chunk:");
+			for (GitDiffChunk c : chunk) {
+				System.out.println(c);
+			}
+		}
+		return chunks;
+	}
+
 }
---
+++ covid-evolution-diff/src/main/java/DiffType.java
@@ -0,0 +1,6 @@
+
+public enum DiffType {
+	ADDITION,
+	DELETION,
+	NEUTRAL
+}
