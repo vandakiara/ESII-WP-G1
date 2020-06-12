8028a7f541733025c27d5fe03911d6d6171cc652
Franciele Faccin <francielefaccin@gmail.com>
2020-06-12 23:45:49 +0100

    Clean up code

--- covid-evolution-diff/src/main/java/ConnGit.java
+++ covid-evolution-diff/src/main/java/ConnGit.java
@@ -2,10 +2,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.lib.ObjectId;
@@ -24,81 +27,80 @@ public class ConnGit {
 	final String REMOTE_URL = "https://github.com/vbasto-iscte/ESII1920.git";
 	final String LOCAL_PATH = "ESII1920";
 	final String FILE_PATH = "covid19spreading.rdf";
-	Repository repository;
-	Git git;
+	private Repository repository;
+	private Git git;
+	private Ref tagLatest;
+	private Ref tagPrevious;
+	OutputStream output = new OutputStream() {
+		private StringBuilder string = new StringBuilder();
 
-	public GitDiff getDiff() {
-		File localPathFile = null;
-		List<List<GitDiffChunk>> parsedDiff = null;
-		try {
-			localPathFile = File.createTempFile(LOCAL_PATH, "");
-			if (!localPathFile.delete()) {
-				throw new IOException("Could not delete temporary file " + LOCAL_PATH);
-			}
-			git = Git.cloneRepository().setDirectory(localPathFile).setURI(REMOTE_URL).call();
-			repository = git.getRepository();
-		} catch (GitAPIException | IOException e) {
-			System.out.println("Failed to read repo at " + REMOTE_URL + ": " + e);
+		@Override
+		public void write(int x) throws IOException {
+			this.string.append((char) x);
 		}
 
-		Object[] tagArray = { null, null };
-		try {
-			tagArray = git.tagList().call().toArray();
+		public String toString() {
+			return this.string.toString();
+		}
+	};
 
+	public boolean hasNewTag() {
+		Collection<Ref> refs;
+		try {
+			refs = Git.lsRemoteRepository().setHeads(true).setTags(true).setRemote(REMOTE_URL).call();
+			return refs.iterator().next().equals(tagLatest);
 		} catch (GitAPIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Failed to check for new tags in " + REMOTE_URL + ": " + e);
 		}
+		return false;
+	}
+
+	public GitDiff getDiff() throws IOException, InvalidRemoteException, TransportException, GitAPIException {
+
+		/**
+		 * Clear to local directory to clone the remote project
+		 */
+		File localPathFile = File.createTempFile(LOCAL_PATH, "");
+		if (!localPathFile.delete()) {
+			throw new IOException("Could not delete temporary file " + LOCAL_PATH);
+		}
+		/**
+		 * Clone project locally
+		 */
+		git = Git.cloneRepository().setDirectory(localPathFile).setURI(REMOTE_URL).call();
+		repository = git.getRepository();
+
+		/**
+		 * Get latest tags
+		 */
+		Object[] tagArray = git.tagList().call().toArray();
 		int size = tagArray.length;
 		Ref tagLatest = size > 0 ? (Ref) tagArray[size - 1] : null;
 		Ref tagPrevious = size > 1 ? (Ref) tagArray[size - 2] : null;
 
-		// the diff works on TreeIterators, we prepare two for the two branches
-		AbstractTreeIterator oldTreeParser = null;
-		AbstractTreeIterator newTreeParser = null;
-		try {
-			newTreeParser = prepareTreeParser(repository, tagLatest.getObjectId().getName());
-			oldTreeParser = prepareTreeParser(repository, tagPrevious.getObjectId().getName());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		/**
+		 * Prepare Tree Iterators to compare version from both tags
+		 */
+		AbstractTreeIterator oldTreeParser = prepareTreeParser(repository, tagLatest.getObjectId().getName());
+		AbstractTreeIterator newTreeParser = prepareTreeParser(repository, tagPrevious.getObjectId().getName());
 
-		// then the porcelain diff-command returns a list of diff entries
-		List<DiffEntry> diff = null;
-		try {
-			diff = git.diff().setOldTree(oldTreeParser).setNewTree(newTreeParser)
-					.setPathFilter(PathFilter.create(FILE_PATH)).
-					// to filter on Suffix use the following instead
-					// setPathFilter(PathSuffixFilter.create(".java")).
-					call();
-		} catch (GitAPIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		/**
+		 * Get list of Diff Entries from comparing the file version between the 2 latest tags
+		 */
+		List<DiffEntry> diff = git.diff().setOldTree(oldTreeParser).setNewTree(newTreeParser)
+				.setPathFilter(PathFilter.create(FILE_PATH)).
+				call();
+
+		/**
+		 * Parse Diff Entries into Git Diff Chunks, that can later be used to generate
+		 * the HTML file containing the Diff
+		 */
+		List<List<GitDiffChunk>> parsedDiff = null;
 		for (DiffEntry entry : diff) {
-			OutputStream output = new OutputStream() {
-				private StringBuilder string = new StringBuilder();
 
-				@Override
-				public void write(int x) throws IOException {
-					this.string.append((char) x);
-				}
-
-				public String toString() {
-					return this.string.toString();
-				}
-			};
-			
 			try (DiffFormatter formatter = new DiffFormatter(output)) {
 				formatter.setRepository(repository);
-
-				try {
-					formatter.format(entry);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				formatter.format(entry);
 				parsedDiff = getDiffChunks(output.toString());
 			}
 		}
@@ -110,8 +112,10 @@ public class ConnGit {
 	}
 
 	private static AbstractTreeIterator prepareTreeParser(Repository repository, String objectId) throws IOException {
-		// from the tag we can, build the tree which allows to construct the
-		// TreeParser for doing the diff
+		/**
+		 *  From the tag we can, build the tree which allows to construct the
+		 *  TreeParser for doing the diff
+		 */
 		try (RevWalk walk = new RevWalk(repository)) {
 			RevCommit commit = walk.parseCommit(ObjectId.fromString(objectId));
 			RevTree tree = walk.parseTree(commit.getTree().getId());
@@ -152,7 +156,7 @@ public class ConnGit {
 				DiffType type = d.indexOf('-') == 0 ? DiffType.DELETION
 						: d.indexOf('+') == 0 ? DiffType.ADDITION : DiffType.NEUTRAL;
 				chunk.add(new GitDiffChunk(countAddition, countDeleted, type, d));
-				
+
 				switch (type) {
 				case DELETION:
 					countDeleted++;
