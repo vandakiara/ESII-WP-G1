4dd50288eafbc38f5282415652dc1f5285714d93
Hugo Barroca <hugobarroca98@gmail.com>
2020-06-14 16:14:46 +0100

    Added bootstrap to HTML table.

--- covid-sci-discoveries/src/test/java/pdf/extractor/PDF_ExtractorTest.java
+++ covid-sci-discoveries/src/test/java/pdf/extractor/PDF_ExtractorTest.java
@@ -15,7 +15,7 @@ import java.io.File;
  */
 public class PDF_ExtractorTest {
 	/** Path to the CSV file where the metadata of the PDF files is stored. */
-	private final File csvPath = new File("\\wordpress\\wp-content\\uploads\\simple-file-list");
+	private final File csvPath = new File("..\\wordpress\\wp-content\\uploads\\simple-file-list");
 
 	/**
 	 * Tests if the constructor is returning anything.
