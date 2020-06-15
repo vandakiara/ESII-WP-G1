c3f25e60c3d92f02ed53cc734cd993ebd7ae8b9a
Franciele Faccin <francielefaccin@gmail.com>
2020-06-15 10:30:33 +0100

    fix for vertx caching test

--- covid-query/src/test/java/CovidQueryMainTest.java
+++ covid-query/src/test/java/CovidQueryMainTest.java
@@ -1,11 +1,21 @@
 
-
 import static org.junit.Assert.assertTrue;
 
+import java.util.Properties;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import pt.iscte.esii.ConnGit;
+
 class CovidQueryMainTest {
-	
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Properties properties = System.getProperties();
+		properties.setProperty("vertx.disableFileCaching", "true");
+	}
+
 	@Test
 	void testMain() {
 		String[] args = null;
